09c494c5473c6d175775459b513e9bf0e497654a
Jonas Stepanik <jonasstepanik@web.de>
2019-04-23 16:30:07 +0000

    Ich habe den Deep Reinforcement Learning Algorithmus Deep Q-Learning mit keras und numpy implementiert. Ich habe bewusst auf eine Reinforcement Learning Library verzichtet, weil ich denke, dass der DQN Algorithmus noch relativ überschaubar ist im Gegensatz zu anderen Deep Reinforcement Learning Algorithmen und alle Beteiligten, nachvollziehen können was genau passiert. Ich habe versucht es so zu implementieren, dass es auch für z.B Bilder als Observation funktioniert, jedoch kann es sein, dass hier und da ein paar Sachen verändert werden müssen, die ich nicht bedacht habe.
    Ich habe z.B. verzichtet ein Target-Network zu implementieren, weil ich denke, dass das Problem auch ohne dieses gelöst werden kann. Es würde den Code nur komplexer machen. Sollte jedoch ein Target-Network benötigt werden, kann dies gerne noch hinzugefügt werden.
    
    Ressourcen:
    DQN Paper: https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf

--- dinoki/rl/model.py
+++ dinoki/rl/model.py
@@ -1,42 +1,146 @@
 """
 Reinforcement learning model for the dino game.
+Deep Q-Learning
 """
 
-import tensorflow as tf
-import trfl
+import random
+import numpy as np
+from collections import deque
 
+class DQN(object):
+    def __init__(
+        self, model, observation_size, action_size, memory_size,
+        gamma=0.95, epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.995
+    ):
+        assert isinstance(observation_size, list)
+        assert isinstance(action_size, int)
+        assert isinstance(memory_size, int)
+        assert isinstance(gamma, float)
+        assert isinstance(epsilon, float)
+        assert isinstance(epsilon_min, float)
+        assert isinstance(epsilon_decay, float)
+        # Shape der Observationen, die das Neuronale Netz bekommt.
+        self.obs_size = observation_size
+        # Wieviele Aktionen ausgeführt werden können.
+        self.act_size = action_size
+        # Neuronales Netz mit keras-api.
+        self.model = model
+        self.assert_model()
+        # Während des Trainings werden immmer Transitions zu dem Replay Buffer 
+        # hinzugefügt. 
+        # Das Neuronale Netz wird ausschließlich aus zufälligen Transitions aus 
+        # dem Replay Buffer trainiert.
+        # Das hilft um das Neuronale Netz Daten Effizient zu machen und das 
+        # Neuronale Netzwerk an alte Transitions zu erinnern.
+        self.memory = deque(maxlen=memory_size)
+        # Die discount rate besimmt wieviel vorherige Erfahrungen gewichtet 
+        # werden sollen.
+        # 1 = max
+        # 0 = min
+        self.gamma = gamma
+        # Die exploration rate gibt an wie random der Agent sich verhalten soll.
+        # 1 = max
+        # 0 = min
+        # Die exploration rate wird im Laufe des Trainings verringert, damit der 
+        # Agent mehr versucht seine momentane Technik zu verbessern, anstatt 
+        # neue Techniken zu suchen.
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
 
-# DUMMY CODE!
+    def assert_model(self):
+        dummy_input = np.ones([1, *self.obs_size])
+        assert self.model.predict(dummy_input).shape == (1, self.act_size)
 
-class DinoRl(object):
-    def __init__(self, learning_rate=0.1):
-        self.learning_rate = learning_rate
+    def add_transition(self, state, action, reward, next_state, done):
+        # TODO: Make memory efficient
+        assert isinstance(state, np.ndarray)
+        assert isinstance(action, (int, float))
+        assert isinstance(reward, (int, float))
+        assert isinstance(next_state, np.ndarray)
+        assert isinstance(done, bool)
+        # Speichert eine Transition in dem Replay Buffer
+        self.memory.append((state, action, reward, next_state, done))
 
-        self.setup_model()
-        self.setup_loss()
-        self.setup_optimizer()
+    def get_action(self, state):
+        assert isinstance(state, np.ndarray)
+        assert len(state.shape) > 1
+        assert state.shape[0] == 1
+        # Epsilon-Greedy
+        # Mit einer Wahrscheinlichkeit von self.epsilon, wähle eine zufällige 
+        # Aktion aus.
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(list(range(self.act_size)))
+        # Mit einer Wahrscheinlichkeit von 1 - self.epsilon, wähle eine Aktion 
+        # basierend der Policy.
+        # Bestimme alle q-values der Aktionen im momentanen state.
+        act_values = self.model.predict(state)
+        # Wähle die Aktion mit dem höchsten q-value
+        return np.argmax(act_values[0])  # returns action
 
-    def setup_model(self):
-        # Q-values for the previous and next timesteps, shape [batch_size,
-        # num_actions].
-        self.q_tm1 = tf.get_variable(
-            "q_tm1", initializer=[[1., 1., 0.], [1., 2., 0.]], dtype=tf.float32)
-        self.q_t = tf.get_variable(
-            "q_t", initializer=[[0., 1., 0.], [1., 2., 0.]], dtype=tf.float32)
+    def train_on_batch(self, batch_size):
+        assert isinstance(batch_size, int)
+        # Sample ein batch aus dem Replay Buffer
+        batch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in batch:
+            state = np.atleast_2d(state)
+            next_state = np.atleast_2d(next_state)
+            # Berechne das eigentliche q-value der Aktion mithilfe der 
+            # Bellman Gleichung.
+            if done:
+                target = reward
+            else:
+                target = reward + self.gamma * np.amax(
+                    self.model.predict(next_state)[0]
+                )
+            # kleiner Trick um die keras fit api zu nutzen
+            target_q = self.model.predict(state)
+            target_q[0][action] = target
+            # Trainiere das Neuronale Netz mit Hilfe des target q-values.
+            self.model.fit(state, target_q, epochs=1, verbose=0)
+        # Update exploration rate
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
-        # Action indices, discounts and rewards, shape [batch_size].
-        self.a_tm1 = tf.constant([0, 1], dtype=tf.int32)
-        self.r_t = tf.constant([1, 1], dtype=tf.float32)
-        self.pcont_t = tf.constant([0, 1],
-                                   dtype=tf.float32)  # the discount factor
+    def load(self, path):
+        assert isinstance(path, str)
+        self.model.load_weights(path)
 
-    def setup_loss(self):
-        # Q-learning loss, and auxiliary data.
-        self.loss, self.q_learning = trfl.qlearning(self.q_tm1, self.a_tm1,
-                                                    self.r_t, self.pcont_t,
-                                                    self.q_t)
-        self.reduced_loss = tf.reduce_mean(self.loss)
-
-    def setup_optimizer(self):
-        self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        self.train_op = self.optimizer.minimize(self.reduced_loss)
+    def save(self, path):
+        assert isinstance(path, str)
+        self.model.save_weights(path)
+        
+if __name__ == "__main__":
+    # Beispiel
+    
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.optimizers import Adam
+    
+    observation_size = 7
+    action_size = 5
+    memory_size = 1000
+    learning_rate = 0.01
+    num_games = 10
+    batch_size = 5
+    
+    model = Sequential()
+    model.add(Dense(24, input_shape=[observation_size,], activation='relu'))
+    model.add(Dense(24, activation='relu'))
+    model.add(Dense(action_size, activation='linear'))
+    model.compile(loss='mse', optimizer=Adam(lr=learning_rate))
+    
+    dqn = DQN(model, [observation_size], action_size, memory_size)
+    
+    print(dqn.get_action(np.ones((1, observation_size))))
+    
+    for _ in range(num_games):
+        dqn.add_transition(
+            np.ones(observation_size), 
+            np.random.randint(action_size), 
+            np.random.randint(-100, 100),
+            np.ones(observation_size),
+            bool(np.random.randint(2))
+        )
+        
+    dqn.train_on_batch(batch_size)
